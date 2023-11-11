187c0beec84b77f333f12d04c03a49232dd9e8a8
monteri <lansevermore>
2023-11-11 18:22:35 +0200

    fix: include newly created files in the list of changes

--- utils/getChangedFiles.js
+++ utils/getChangedFiles.js
@@ -3,8 +3,7 @@ const simpleGit = require('simple-git');
 function getChangedFiles(callback) {
   const git = simpleGit();
 
-  // Use the 'all' option to include untracked files
-  git.status(['-u', 'all'], (err, status) => {
+  git.status((err, status) => {
     if (err) {
       console.error('An error occurred while checking Git status:', err.message);
       process.exit(1);
@@ -13,6 +12,8 @@ function getChangedFiles(callback) {
     const changedFiles = status.files
       .filter((file) => file.working_dir !== ' ' && file.working_dir !== '?')
       .map((file) => file.path);
+
+    changedFiles.push(...status.created);
 
     callback(changedFiles);
   });
